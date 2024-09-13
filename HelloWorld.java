7eed99cb53d7136602fe93d2a9885f7b2ebaea18
Wells Hull <wellington.hull@icloud.com>
2024-09-12 20:56:52 -0500

    Changes by Wells

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ class Test
     public static void main(String []args)
     {
         System.out.println("Hello World!, 0 years at Beloit");
+        System.out.println("Hello its me Wells");
     }
 };
